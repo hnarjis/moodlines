37a2d8bdf012743237b0a4ab065ad2c58bb2f35f
Narjis Hachilif <hnarjis@gmail.com>
2015-06-24 00:10:31 +0200

    Adding controller and routes

---
+++ app/controllers/input_controller.rb
@@ -0,0 +1,25 @@
+class InputController < ApplicationController
+  def index
+    day = Mood.order(:created_at).first.created_at.yesterday.strftime('%F')
+    happiness = 0
+    energy = 0
+    motivation = 0
+    @moods = [['Day', 'Happiness', 'Energy', 'Motivation'],[day, happiness, energy, motivation]]
+    Mood.all.order(:created_at).each do |mood|
+      day = mood.created_at.strftime('%F')
+      happiness += mood.happiness
+      energy += mood.energy
+      motivation += mood.motivation
+      @moods << [day, happiness, energy, motivation]
+    end
+  end
+
+  def create
+    @mood = Mood.new(happiness: params.require(:happiness),
+                     energy: params.require(:energy),
+                     motivation: params.require(:motivation))
+
+    @mood.save
+    redirect_to :back
+  end
+end
