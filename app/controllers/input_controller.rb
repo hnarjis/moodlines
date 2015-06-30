f24e5939a67f2f1c442e235fccbcd796e6bd23aa
Narjis Hachilif <hnarjis@gmail.com>
2015-06-30 23:16:33 +0200

    MVC refactoring

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -1,26 +1,15 @@
 class InputController < ApplicationController
   def index
-    @moods = []
-    first_mood = current_user.moods.order(:created_at).first
-    return if first_mood.nil?
-    day = first_mood.created_at.yesterday.strftime('%F')
-    happiness = 0
-    energy = 0
-    motivation = 0
-    @moods = [['Day', 'Happiness', 'Energy', 'Motivation'],[day, happiness, energy, motivation]]
-    current_user.moods.all.order(:created_at).each do |mood|
-      day = mood.created_at.strftime('%F')
-      happiness += mood.happiness
-      energy += mood.energy
-      motivation += mood.motivation
-      @moods << [day, happiness, energy, motivation]
-    end
+    @done_for_today = current_user.done_for_today?
+    @moods = current_user.mood_list
+    @has_moods = !@moods.empty?
+    @moods.prepend(['Day', 'Happiness', 'Energy', 'Motivation'])
   end
 
   def create
     @mood = current_user.moods.new(happiness: params.require(:happiness),
-                     energy: params.require(:energy),
-                     motivation: params.require(:motivation))
+                                   energy: params.require(:energy),
+                                   motivation: params.require(:motivation))
 
     @mood.save
     redirect_to :back
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,31 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :moods
+
+  def done_for_today?
+    moods.order(:created_at).last.created_at.to_date == Date.today
+  end
+
+  def first_mood
+    moods.order(:created_at).first
+  end
+
+  def mood_list
+    if first_mood.nil?
+      return []
+    end
+    day = first_mood.created_at.yesterday.strftime('%F')
+    happiness = 0
+    energy = 0
+    motivation = 0
+    result = [[day, happiness, energy, motivation]]
+    moods.all.order(:created_at).each do |mood|
+      day = mood.created_at.strftime('%F')
+      happiness += mood.happiness
+      energy += mood.energy
+      motivation += mood.motivation
+      result << [day, happiness, energy, motivation]
+    end
+    return result
+  end
 end
